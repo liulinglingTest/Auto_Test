552929f00d301b8ad2b1ac04d12f4a52d44befb4
liull <1833915972@qq.com>
2021-10-22 16:00:19 +0800

    增加LanaDigital贷后接口自动化测试

--- api_auto_test/TestCase/Ld_DaiHou_Api_Test.py
+++ api_auto_test/TestCase/Ld_DaiHou_Api_Test.py
@@ -12,8 +12,22 @@ class DaiHou_Api_Test(unittest.TestCase):
         print('setup_test')
     def tearDown(self):  # 每个用例运行之后运行的
         print('teardown_test')
+    def test_credit_payment(self):
+        '''【LanaDigital】/api/credit/payment用户提现接口-正案例'''
+    def test_credit_payment_delay(self):
+        '''【LanaDigital】/api/credit/payment/delay_payout_handler用户提现接口(延迟放款)-正案例'''
     def test_payment_detail(self):
-        '''【LanaDigital】/api/cust/login注册登录接口-正案例'''
+        '''【LanaDigital】/api/credit/payment/detail提现详情接口-正案例'''
+    def test_hook_payment(self):
+        '''【LanaDigital】/api/hook/payment支付还款回调接口-正案例'''
+    def test_hook_repayment(self):
+        '''【LanaDigital】/api/hook/repayment用户还款回调接口-正案例'''
+    def test_hook_repayment_cancel(self):
+        '''【LanaDigital】/api/hook/repayment/cancel_repayment_orders取消还款订单接口-正案例'''
+    def test_hook_repayment_cancel(self):
+        '''【LanaDigital】/api/hook/repayment/cancel_repayment_orders取消还款订单接口-正案例'''
+    def test_hook_repay_apply_collection(self):
+        '''【LanaDigital】/api/hook/repay/apply/collection催收还款码获取接口-正案例'''
 
     @classmethod
     def tearDownClass(cls): # 在所有用例都执行完之后运行的
