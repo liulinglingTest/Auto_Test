80701907829688e9079a348281821b03d152ba40
liull <1833915972@qq.com>
2021-11-23 18:30:01 +0800

    1123-增加LanaDigital贷前接口

--- api_auto_test/TestCase/Ld_DaiQian_Api_Test.py
+++ api_auto_test/TestCase/Ld_DaiQian_Api_Test.py
@@ -155,7 +155,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         data={"custNo":custNo,"contacts":[{"custNo":custNo,"name":"Advance Talktime","phone":"52141","relationship":"10110004","relationshipName":"Hermanos"},{"custNo":custNo,"name":"Cricket","phone":"543212601","relationship":"10110001","relationshipName":"Padres"}]}
         r=requests.post(host_api+'/api/cust/auth/other/contact',data=json.dumps(data),headers=head)#最后一步，填写2个联系人的联系方式
         t=r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'],0)
         # sql = "select OTHER_CONTACT_AUTH from cu_cust_auth_dtl  where CUST_NO='" + custNo + "';"  # cu_客户认证信息明细表
         # other_contact_auth = DataBase(which_db).get_one(sql)
@@ -191,7 +191,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         data = {"bankCode": "10020037", "clabe": "138455214411441118", "custNo": ''}
         r = requests.post(host_api + '/api/cust/auth/bank', data=json.dumps(data), headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 30001)
         self.assertEqual(t['message'], 'custNoParámetro anormal ')
     def test_feedback_record(self):
@@ -203,7 +203,7 @@ class DaiQian_Api_Test(unittest.TestCase):
                'feedbackOption':(None,'No sé cuál es mi cuenta CLABE'),'imgs':('key.png',open(r'D:\pic\app.png', 'rb'),'text/plain')}
         r=requests.post(host_api+"/api/hook/feedback",files=files,headers=headt_api_f)
         t=r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'],0)
     def test_risk_credit(self):
         '''【LanaDigital】/api/task/risk/credit风控授信接口-正案例'''
@@ -217,7 +217,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api=login_code(registNo)
         r=requests.get(host_api+"/api/common/bank/codes",headers=headt_api)
         t=r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'],0)
         self.assertEqual(t,{"data":{"bankList":[{"bankCode":"10020037","bankName":"ABC CAPITAL","preNums":"138"},{"bankCode":"10020020","bankName":"ACCENDO BANCO","preNums":"102"},{"bankCode":"10020034","bankName":"ACTINVER","preNums":"133"},{"bankCode":"10020018","bankName":"AFIRME","preNums":"062"},{"bankCode":"10020067","bankName":"AKALA","preNums":"638"},{"bankCode":"10020021","bankName":"AMERICAN EXPRES","preNums":"103"},{"bankCode":"10020030","bankName":"AUTOFIN","preNums":"128"},{"bankCode":"10020029","bankName":"AZTECA","preNums":"127"},{"bankCode":"10020011","bankName":"BAJIO","preNums":"030"},{"bankCode":"10020007","bankName":"BANAMEX","preNums":"002"},{"bankCode":"10020047","bankName":"BANCO FINTERRA","preNums":"154"},{"bankCode":"10020052","bankName":"BANCO S3","preNums":"160"},{"bankCode":"10020001","bankName":"BANCOMEXT","preNums":"006"},{"bankCode":"10020036","bankName":"BANCOPPEL","preNums":"137"},{"bankCode":"10020046","bankName":"BANCREA","preNums":"152"},{"bankCode":"10020003","bankName":"BANJERCITO","preNums":"019"},{"bankCode":"10020022","bankName":"BANK OF AMERICA","preNums":"106"},{"bankCode":"10020042","bankName":"BANKAOOL","preNums":"147"},{"bankCode":"10020002","bankName":"BANOBRAS","preNums":"009"},{"bankCode":"10020019","bankName":"BANORTE","preNums":"072"},{"bankCode":"10020015","bankName":"BANREGIO","preNums":"058"},{"bankCode":"10020005","bankName":"BANSEFI","preNums":"166"},{"bankCode":"10020017","bankName":"BANSI","preNums":"060"},{"bankCode":"10020031","bankName":"BARCLAYS","preNums":"129"},{"bankCode":"10020041","bankName":"BBASE","preNums":"145"},{"bankCode":"10020008","bankName":"BBVA BANCOMER","preNums":"012"},{"bankCode":"10020025","bankName":"BMONEX","preNums":"112"},{"bankCode":"10020075","bankName":"CAJA POP MEXICA","preNums":"677"},{"bankCode":"10020077","bankName":"CAJA TELEFONIST","preNums":"683"},{"bankCode":"10020063","bankName":"CB INTERCAM","preNums":"630"},{"bankCode":"10020064","bankName":"CI BOLSA","preNums":"631"},{"bankCode":"10020040","bankName":"CIBANCO","preNums":"143"},{"bankCode":"10020032","bankName":"COMPARTAMOS","preNums":"130"},{"bankCode":"10020038","bankName":"CONSUBANCO","preNums":"140"},{"bankCode":"10020071","bankName":"CREDICAPITAL","preNums":"652"},{"bankCode":"10020028","bankName":"CREDIT SUISSE","preNums":"126"},{"bankCode":"10020076","bankName":"CRISTOBAL COLON","preNums":"680"},{"bankCode":"10020083","bankName":"CoDi Valida","preNums":"903"},{"bankCode":"10020027","bankName":"DEUTSCHE","preNums":"124"},{"bankCode":"10020045","bankName":"DONDE","preNums":"151"},{"bankCode":"10020057","bankName":"ESTRUCTURADORES","preNums":"606"},{"bankCode":"10020070","bankName":"EVERCORE","preNums":"648"},{"bankCode":"10020060","bankName":"FINAMEX","preNums":"616"},{"bankCode":"10020065","bankName":"FINCOMUN","preNums":"634"},{"bankCode":"10020081","bankName":"FOMPED","preNums":"689"},{"bankCode":"10020079","bankName":"FONDO (FIRA)","preNums":"685"},{"bankCode":"10020054","bankName":"GBM","preNums":"601"},{"bankCode":"10020066","bankName":"HDI SEGUROS","preNums":"636"},{"bankCode":"10020006","bankName":"HIPOTECARIA FED","preNums":"168"},{"bankCode":"10020010","bankName":"HSBC","preNums":"021"},{"bankCode":"10020048","bankName":"ICBC","preNums":"155"},{"bankCode":"10020012","bankName":"INBURSA","preNums":"036"},{"bankCode":"10020082","bankName":"INDEVAL","preNums":"902"},{"bankCode":"10020044","bankName":"INMOBILIARIO","preNums":"150"},{"bankCode":"10020035","bankName":"INTERCAM BANCO","preNums":"136"},{"bankCode":"10020080","bankName":"INVERCAP","preNums":"686"},{"bankCode":"10020016","bankName":"INVEX","preNums":"059"},{"bankCode":"10020024","bankName":"JP MORGAN","preNums":"110"},{"bankCode":"10020074","bankName":"LIBERTAD","preNums":"670"},{"bankCode":"10020055","bankName":"MASARI","preNums":"602"},{"bankCode":"10020013","bankName":"MIFEL","preNums":"042"},{"bankCode":"10020051","bankName":"MIZUHO BANK","preNums":"158"},{"bankCode":"10020053","bankName":"MONEXCB","preNums":"600"},{"bankCode":"10020023","bankName":"MUFG","preNums":"108"},{"bankCode":"10020033","bankName":"MULTIVA BANCO","preNums":"132"},{"bankCode":"10020059","bankName":"MULTIVA CBOLSA","preNums":"613"},{"bankCode":"10020004","bankName":"NAFIN","preNums":"135"},{"bankCode":"10020043","bankName":"PAGATODO","preNums":"148"},{"bankCode":"10020062","bankName":"PROFUTURO","preNums":"620"},{"bankCode":"10020049","bankName":"SABADELL","preNums":"156"},{"bankCode":"10020009","bankName":"SANTANDER","preNums":"014"},{"bankCode":"10020084","bankName":"SANTANDER2","preNums":"814"},{"bankCode":"10020014","bankName":"SCOTIABANK","preNums":"044"},{"bankCode":"10020050","bankName":"SHINHAN","preNums":"157"},{"bankCode":"10020069","bankName":"STP","preNums":"646"},{"bankCode":"10020078","bankName":"TRANSFER","preNums":"684"},{"bankCode":"10020072","bankName":"UNAGRA","preNums":"656"},{"bankCode":"10020061","bankName":"VALMEX","preNums":"617"},{"bankCode":"10020056","bankName":"VALUE","preNums":"605"},{"bankCode":"10020026","bankName":"VE POR MAS","preNums":"113"},{"bankCode":"10020058","bankName":"VECTOR","preNums":"608"},{"bankCode":"10020039","bankName":"VOLKSWAGEN","preNums":"141"}],"hotBankList":[{"bankCode":"10020008","bankName":"BBVA BANCOMER","preNums":"012"},{"bankCode":"10020029","bankName":"AZTECA","preNums":"127"},{"bankCode":"10020007","bankName":"BANAMEX","preNums":"002"},{"bankCode":"10020009","bankName":"SANTANDER","preNums":"014"},{"bankCode":"10020019","bankName":"BANORTE","preNums":"072"},{"bankCode":"10020010","bankName":"HSBC","preNums":"021"},{"bankCode":"10020014","bankName":"SCOTIABANK","preNums":"044"},{"bankCode":"10020069","bankName":"STP","preNums":"646"}]},"errorCode":0,"message":"ok"})
     def test_feedback_codes(self):
@@ -253,7 +253,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api = login_code(registNo)
         r = requests.get(host_api + '/api/common/code/CONTACT_TYPE', headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 0)
         self.assertEqual(str(t['data']),"[{'codeName': 'Hermanos', 'codeValue': '10110004', 'note': None}, {'codeName': 'Hijos', 'codeValue': '10110003', 'note': None}, {'codeName': 'Padres', 'codeValue': '10110001', 'note': None}, {'codeName': 'Pareja', 'codeValue': '10110002', 'note': None}, {'codeName': 'uno mismo', 'codeValue': '10110005', 'note': None}]")
     def test_get_education_codes(self):
@@ -262,7 +262,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api = login_code(registNo)
         r = requests.get(host_api + '/api/common/code/EDUCATION', headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 0)
         self.assertEqual(str(t['data']),"[{'codeName': 'Licenciatura', 'codeValue': '10190005', 'note': None}, {'codeName': 'No Escolaridad', 'codeValue': '10190001', 'note': None}, {'codeName': 'Posgrado / Maestria', 'codeValue': '10190006', 'note': None}, {'codeName': 'Preparatoria/ Bachillerato', 'codeValue': '10190004', 'note': None}, {'codeName': 'Primaria', 'codeValue': '10190002', 'note': None}, {'codeName': 'Secundaria', 'codeValue': '10190003', 'note': None}]")
     def test_get_jobType_codes(self):
@@ -271,7 +271,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api = login_code(registNo)
         r = requests.get(host_api + '/api/common/code/JOB_TYPE', headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 0)
         self.assertEqual(str(t['data']),"[{'codeName': 'Desempleado', 'codeValue': '10130006', 'note': None}, {'codeName': 'Empleado', 'codeValue': '10130001', 'note': None}, {'codeName': 'Empresario', 'codeValue': '10130002', 'note': None}, {'codeName': 'Estudiante', 'codeValue': '10130004', 'note': None}, {'codeName': 'Independiente', 'codeValue': '10130003', 'note': None}, {'codeName': 'Jubilado', 'codeValue': '10130005', 'note': None}]")
     def test_get_income_codes(self):
@@ -280,7 +280,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api = login_code(registNo)
         r = requests.get(host_api + '/api/common/code/INCOME', headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 0)
         self.assertEqual(str(t['data']),"[{'codeName': '$12000 - $24000', 'codeValue': '10870004', 'note': None}, {'codeName': '$3000 - $6000', 'codeValue': '10870002', 'note': None}, {'codeName': '$6000 - $12000', 'codeValue': '10870003', 'note': None}, {'codeName': 'Menos de $3000', 'codeValue': '10870001', 'note': None}, {'codeName': 'Más de $24000', 'codeValue': '10870005', 'note': None}]")
     def test_get_industry_codes(self):
@@ -289,7 +289,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api = login_code(registNo)
         r = requests.get(host_api + '/api/common/code/INDUSTRY', headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 0)
         self.assertEqual(str(t['data']),"[{'codeName': 'Administrativo', 'codeValue': '10860001', 'note': None}, {'codeName': 'Ayudante en General', 'codeValue': '10860004', 'note': None}, {'codeName': 'Call center', 'codeValue': '10860005', 'note': None}, {'codeName': 'Comerciante', 'codeValue': '10860015', 'note': None}, {'codeName': 'Construcción', 'codeValue': '10860008', 'note': None}, {'codeName': 'Dependencias del Gobierno', 'codeValue': '10860007', 'note': None}, {'codeName': 'Director - Gerente - Encargado', 'codeValue': '10860006', 'note': None}, {'codeName': 'Finanzas - Contador - Agobado', 'codeValue': '10860009', 'note': None}, {'codeName': 'Ingeniero - Arquitecto', 'codeValue': '10860002', 'note': None}, {'codeName': 'Negocio propio', 'codeValue': '10860014', 'note': None}, {'codeName': 'Operario de Transporte', 'codeValue': '10860003', 'note': None}, {'codeName': 'Otros', 'codeValue': '10860016', 'note': None}, {'codeName': 'Recepcionista', 'codeValue': '10860010', 'note': None}, {'codeName': 'Repartidor de plataforma', 'codeValue': '10860012', 'note': None}, {'codeName': 'Servidor Público', 'codeValue': '10860011', 'note': None}, {'codeName': 'Transporte privado', 'codeValue': '10860013', 'note': None}]")
     def test_get_fileType_codes(self):
@@ -298,7 +298,7 @@ class DaiQian_Api_Test(unittest.TestCase):
         headt_api = login_code(registNo)
         r = requests.get(host_api + '/api/common/code/FILE_TYPE', headers=headt_api)
         t = r.json()
-        print(t)
+        # print(t)
         self.assertEqual(t['errorCode'], 0)
         self.assertEqual(str(t['data']),"[{'codeName': 'Anverso de la tarjeta Aadhaar', 'codeValue': '10070015', 'note': None}, {'codeName': 'Anverso de la tarjeta de identificación', 'codeValue': '10070001', 'note': None}, {'codeName': 'Datos brutos de API konjac', 'codeValue': '10070008', 'note': None}, {'codeName': 'Datos de informes de API konjac', 'codeValue': '10070009', 'note': None}, {'codeName': 'Foto de mosaico A_card', 'codeValue': '10070014', 'note': None}, {'codeName': 'Foto personal', 'codeValue': '10070004', 'note': None}, {'codeName': 'Foto viva', 'codeValue': '10070032', 'note': None}, {'codeName': 'Frente de PAN', 'codeValue': '10070017', 'note': None}, {'codeName': 'Historial de llamadas telefónicas API', 'codeValue': '10070010', 'note': None}, {'codeName': 'Información de contactos telefónicos', 'codeValue': '10070023', 'note': None}, {'codeName': 'Información de la aplicación móvil', 'codeValue': '10070012', 'note': None}, {'codeName': 'Información del dispositivo telefónico', 'codeValue': '10070021', 'note': None}, {'codeName': 'Información del teléfono', 'codeValue': '10070007', 'note': None}, {'codeName': 'Informe de crédito CRIF', 'codeValue': '10070030', 'note': None}, {'codeName': 'Informe de crédito de NBFC', 'codeValue': '10070027', 'note': None}, {'codeName': 'Libreta de direcciones local', 'codeValue': '10070006', 'note': None}, {'codeName': 'Lista de llamadas locales', 'codeValue': '10070005', 'note': None}, {'codeName': 'Lista de llamadas telefónicas API', 'codeValue': '10070011', 'note': None}, {'codeName': 'Prueba de empleo', 'codeValue': '10070018', 'note': None}, {'codeName': 'Prueba de ingresos', 'codeValue': '10070019', 'note': None}, {'codeName': 'Resultado de búsqueda de caras', 'codeValue': '10070013', 'note': None}, {'codeName': 'Resultado de comprobación simple de A_card', 'codeValue': '10070025', 'note': None}, {'codeName': 'Resultado de la verificación de la tarjeta A', 'codeValue': '10070026', 'note': None}, {'codeName': 'Resultado de la verificación de la tarjeta PAN', 'codeValue': '10070024', 'note': None}, {'codeName': 'Resultado de la verificación de la tarjeta bancaria', 'codeValue': '10070028', 'note': None}, {'codeName': 'Reverso de la tarjeta Aadhaar', 'codeValue': '10070016', 'note': None}, {'codeName': 'Reverso de la tarjeta de identificación', 'codeValue': '10070002', 'note': None}, {'codeName': 'SMS', 'codeValue': '10070031', 'note': None}, {'codeName': 'Solicitud de préstamos', 'codeValue': '10070029', 'note': None}, {'codeName': 'Un frente de tarjeta de pasaporte tripartito', 'codeValue': '10070020', 'note': None}, {'codeName': 'nformación de ubicación GPS', 'codeValue': '10070022', 'note': None}, {'codeName': 'prueba de reembolso', 'codeValue': '10070035', 'note': None}, {'codeName': 'tarjeta bancaria', 'codeValue': '10070003', 'note': None}]")
     @classmethod
